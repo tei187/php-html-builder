d8d6d32af8696fd8b2dbea9a40fd882c259b7618
tei187 <bonk.piotr@gmail.com>
2022-11-24 10:14:58 +0100

    Loader fix

--- loader.php
+++ loader.php
@@ -4,8 +4,19 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 require_once __DIR__ . "/src/Constants/Doctype.php";
 require_once __DIR__ . "/src/Constants/Charset.php";
+
 require_once __DIR__ . "/src/Traits/ListableAttributes.php";
 require_once __DIR__ . "/src/Traits/ClosingTags.php";
+
 require_once __DIR__ . "/src/HTML.php";
+require_once __DIR__ . "/src/Head.php";
+require_once __DIR__ . "/src/OpenGraph.php";
+require_once __DIR__ . "/src/OpenGraph/Album.php";
+require_once __DIR__ . "/src/OpenGraph/Article.php";
+require_once __DIR__ . "/src/OpenGraph/Audio.php";
+require_once __DIR__ . "/src/OpenGraph/Image.php";
+require_once __DIR__ . "/src/OpenGraph/Profile.php";
+require_once __DIR__ . "/src/OpenGraph/Song.php";
+require_once __DIR__ . "/src/OpenGraph/Video.php";
 require_once __DIR__ . "/src/Form.php";
 require_once __DIR__ . "/src/Bootstrap.php";
--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$o = ['test' => 1, "test2" => 2];
-
-print_r(array_merge($o, ['test2' => null]));
