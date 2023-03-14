3844d3fd883cf2fbc5d475063036f6a94dc4672c
tei187 <bonk.piotr@gmail.com>
2023-03-14 09:21:35 +0100

    attributes handler for Dropdown in Bootstrap

--- src/Bootstrap/Dropdown/Dropdown.php
+++ src/Bootstrap/Dropdown/Dropdown.php
@@ -2,7 +2,6 @@
 
 namespace tei187\HTMLBuilder\Bootstrap\Dropdown;
 
-use tei187\HTMLBuilder\Bootstrap;
 use tei187\HTMLBuilder\HTML;
 use tei187\HTMLBuilder\Bootstrap\Dropdown\Button;
 use tei187\HTMLBuilder\Bootstrap\Dropdown\Menu\ListDropdownMenu;
@@ -18,8 +17,16 @@ class Dropdown {
      * @return string
      */
     public static function Dropdown(array $attributes = [], Button $button, $menu) : string {
+        if(isset($attributes['class'])) {
+            preg_match('/(?:\s+|^)dropdown(?:\s+|$)/', $attributes['class']) === 0
+                ? $attributes['class'] = "dropdown " . $attributes['class']
+                : null;
+        } else {
+            $attributes['class'] = 'dropdown';
+        }
+        
         return
-            HTML::Div(['class' => 'dropdown'],
+            HTML::Div($attributes,
                 $button->render() . $menu->render()
         );
     }
